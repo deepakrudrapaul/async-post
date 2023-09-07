9078ad80528d9badf8d64b32a52fb722662fe4ae
Deepak Paul <deepakrudrapaul@gmail.com>
2023-09-08 00:41:11 +0530

    feat: add post form

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,16 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { matAddAPhotoOutline, matAddCircleOutlineOutline } from '@ng-icons/material-icons/outline';
+import {
+  matAddAPhotoOutline,
+  matAddCircleOutlineOutline,
+  matCloseOutline,
+} from '@ng-icons/material-icons/outline';
+import { FormsModule, NgForm } from '@angular/forms';
+
+interface Post {
+  content: string;
+}
+interface PostForm {
+  post: Post[];
+}
 
 @Component({
   selector: 'app-post-form',
   standalone: true,
-  imports: [CommonModule, NgIconComponent],
-  providers:[provideIcons({ matAddAPhotoOutline, matAddCircleOutlineOutline })],
+  imports: [CommonModule, NgIconComponent, FormsModule],
+  providers: [
+    provideIcons({
+      matAddAPhotoOutline,
+      matAddCircleOutlineOutline,
+      matCloseOutline,
+    }),
+  ],
   templateUrl: './post-form.component.html',
-  styleUrls: ['./post-form.component.css']
+  styleUrls: ['./post-form.component.css'],
 })
-export class PostFormComponent {
+export class PostFormComponent implements OnInit {
+  postForm: PostForm = {
+    post: [],
+  };
 
+  ngOnInit(): void {
+    this.addPost();
+  }
+
+  handleSubmit(form: NgForm) {}
+
+  addPost(): void {
+    this.postForm.post.push({ content: '' });
+  }
+
+  removePost(position: number): void {
+    let post = this.postForm.post.filter((post, index) => position !== index);
+    this.postForm.post = post;
+  }
 }
